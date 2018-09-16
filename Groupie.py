35e4721e445324fb69f4f3c50e7a44a9c0c78188
Shandon Anderson <andersonshandon@gmail.com>
2018-09-16 00:56:56 -0400

    TEST7

--- Groupie.py
+++ Groupie.py
@@ -44,7 +44,7 @@ class Bot(object):
         r = requests.post('https://api.groupme.com/v3/bots/post', data = postJson)
 
 
-api = API()
-bot = Bot()
+# api = API()
+# bot = Bot()
 # api.loadJson('quotes.json')
-bot.postQuote(api.getQuote(api.loadJson('quotes.json')))
+# bot.postQuote(api.getQuote(api.loadJson('quotes.json')))
