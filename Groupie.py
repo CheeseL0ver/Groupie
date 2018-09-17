70b41b76484e6efe9b158ea8b0e9256af732aed2
Shandon Anderson <andersonshandon@gmail.com>
2018-09-17 15:01:45 -0400

    Additon of weather functionality fixes

--- Groupie.py
+++ Groupie.py
@@ -303,8 +303,8 @@ class API(object):
         self.cloudCov = ''
 
         url = 'http://api.openweathermap.org/data/2.5/weather'
-            +'?zip={}&units=imperial&APPID={}'.format(
-            WEATHER_ZIP_CODE,WEATHER_API_KEY)
+        +'?zip={}&units=imperial&APPID={}'.format(
+        WEATHER_ZIP_CODE,WEATHER_API_KEY)
 
         data = self.loadJson(self.fetchJson(url))
 
